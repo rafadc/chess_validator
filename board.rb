460ae460fba3172d86dcf1ec294f67a19907aa4b
alfredo <alfredo@localhost.localdomain>
2014-09-18 16:22:33 +0200

    add ckecking free destination, but buggy

--- board.rb
+++ board.rb
@@ -1,20 +1,27 @@
+require 'pry'
+
 class ChessBoard
   def initialize
-    @board = {"a1" => "wR", "b1" => "wN", "c1" => "wB" , "d1" => "wQ", "e1" => "wK", "f1" => "wB", "g1" => "wN", "h1" => "wR",
-              "a2" => "wP", "b2" => "wP", "c2" => "wP" , "d2" => "wP", "e2" => "wP", "f2" => "wP", "g2" => "wP", "h2" => "wP",
-              "a8" => "bR", "b8" => "bN", "c8" => "bB" , "d8" => "bQ", "e8" => "bK", "f8" => "bB", "g8" => "bN", "h8" => "bR", 
-              "a7" => "bP", "b7" => "bP", "c7" => "bP" , "d7" => "bP", "e7" => "bP", "f7" => "bP", "g7" => "bP", "h7" => "bP"}
+    @board = {
+      "a8" => "bR", "b8" => "bN", "c8" => "bB" , "d8" => "bQ", "e8" => "bK", "f8" => "bB", "g8" => "bN", "h8" => "bR", 
+      "a7" => "bP", "b7" => "bP", "c7" => "bP" , "d7" => "bP", "e7" => "bP", "f7" => "bP", "g7" => "bP", "h7" => "bP",
+      "a6" => "--", "b6" => "--", "c6" => "--" , "d6" => "--", "e6" => "--", "f6" => "--", "g6" => "--", "h6" => "--",
+      "a5" => "--", "b5" => "--", "c5" => "--" , "d5" => "--", "e5" => "--", "f5" => "--", "g5" => "--", "h5" => "--",
+      "a4" => "--", "b4" => "--", "c4" => "--" , "d4" => "--", "e4" => "--", "f4" => "--", "g4" => "--", "h4" => "--",
+      "a3" => "--", "b3" => "--", "c3" => "--" , "d3" => "--", "e3" => "--", "f3" => "--", "g3" => "--", "h3" => "--",
+      "a2" => "wP", "b2" => "wP", "c2" => "wP" , "d2" => "wP", "e2" => "wP", "f2" => "wP", "g2" => "wP", "h2" => "wP",
+      "a1" => "wR", "b1" => "wN", "c1" => "wB" , "d1" => "wQ", "e1" => "wK", "f1" => "wB", "g1" => "wN", "h1" => "wR"
+            }
   end
-  def check(move)
+
+  def check_move(move)
     origin = move.slice(0,2)
-    destination = move.slice(0,3)
-    return BlackPawn.new.can_move?(origin, destination) if  @board[origin]=="bP"
-    return WhitePawn.new.can_move?(origin,destination) if  @board[origin]=="wP"
-    return Knight.new.can_move?()
+    destination = move.slice(3,2)
+    Piece.new(@board, origin, destination).can_move? ? "LEGAL" : "ILLEGAL"
   end
+
+
 end
-
-
 # class Rook
 #   def can_move?(origin,destination)
 #     origin_row = origin.slice(1,1)
@@ -24,25 +31,54 @@ end
 #     result = (destination_row == origin_row) || (destination_col == origin_col)
 #   end
 # end
+class Piece
+
+  def initialize(board, origin, destination)
+    @board = board
+    @destination = destination
+    @origin = origin
+  end
+
+  def destination_free
+    @board[@destination] == "--"
+  end
+
+  def can_move?
+binding.pry
+    return BlackPawn.new.can_move?(@origin, @destination) if @board[@origin]=="bP" && destination_free
+    return WhitePawn.new.can_move?(@origin, @destination) if @board[@origin]=="wP" && destination_free
+    return Knight.new.can_move?(@origin, @destination) if ["wN", "bN"].include? @board[@origin] && destination_free
+  end
+end
+
 
 class WhitePawn
   def can_move?(origin, destination)
     origin_row = origin.slice(1,1).to_i
+    origin_col = origin.slice(0,1)
     destination_row = destination.slice(1,1).to_i
-    result = (destination_row == origin_row+1 && origin_row > 2) || (destination_row <= origin_row+2 && origin_row == 2)
+    destination_col = destination.slice(0,1)
+    result =  (origin_col == destination_col) && 
+        ((destination_row == origin_row+1 && origin_row > 2) || 
+        (destination_row <= origin_row+2 && origin_row == 2))
   end
 end
 
 class BlackPawn
   def can_move?(origin, destination)
     origin_row = origin.slice(1,1).to_i
+    origin_col = origin.slice(0,1)
     destination_row = destination.slice(1,1).to_i
-    result = (destination_row == origin_row-1 && origin_row < 7) || (destination_row >= origin_row-2 && origin_row == 7)
+    destination_col = destination.slice(0,1)
+    result =  (origin_col == destination_col) && 
+            ((destination_row == origin_row-1 && origin_row < 7) || 
+              (destination_row >= origin_row-2 && origin_row == 7))
   end
 end
 
 class Knight
   def can_move?(origin, destination)
+binding.pry
     origin_row = origin.slice(1,1).to_i
     origin_col = origin.slice(0,1).ord
     destination_row = destination.slice(1,1).to_i
--- test/test.rb
+++ test/test.rb
@@ -1,16 +1,24 @@
 require_relative '../board'
 
+
 # wp = WhitePawn.new
 # puts wp.can_move?("a2","a3")
 # puts wp.can_move?("a2","a4")
 # puts wp.can_move?("a2","a5")
 
-# bp = BlackPawn.new
-# puts bp.can_move?("a7","a6")
+ # bp = BlackPawn.new
+ # puts bp.can_move?("a7","b6")
 # puts bp.can_move?("a7","a5")
 # puts bp.can_move?("a7","a4")
 
-# board1 = ChessBoard.new
+board1 = ChessBoard.new
+
+# smoves = File.open('simple_moves.txt', 'r')
+# smoves.each do |x|
+#   puts board1.check_move(x)
+# end
+# smoves.close
+
 # puts board1.check("a7 a6")
 # puts board1.check("a7 a5")
 # puts board1.check("a7 a4")
@@ -18,22 +26,18 @@ require_relative '../board'
 # puts board1.check("a2 a4")
 # puts board1.check("a2 a5")
 
-horse = Knight.new
+# horse = Knight.new
 
-puts horse.can_move?("e5", "f7")
-puts horse.can_move?("e5", "g6")
-puts horse.can_move?("e5", "g4")
-puts horse.can_move?("e5", "f3")
-puts horse.can_move?("e5", "d3")
-puts horse.can_move?("e5", "c4")
-puts horse.can_move?("e5", "d7")
-puts horse.can_move?("e5", "c6")
-
-
-puts horse.can_move?("e5", "g9")
+# puts horse.can_move?("b1", "c3")
+# puts horse.can_move?("e5", "g6")
+# puts horse.can_move?("e5", "g4")
+# puts horse.can_move?("e5", "f3")
+# puts horse.can_move?("e5", "d3")
+# puts horse.can_move?("e5", "c4")
+# puts horse.can_move?("e5", "d7")
+# puts horse.can_move?("e5", "c6")
 
 
+# puts horse.can_move?("e5", "g9")
 
-
-
-
+puts board1.check_move("b8 a6")
