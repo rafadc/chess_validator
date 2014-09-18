c6b23fa22cdcc5d246c728405aa7670aa0912784
Rafa de Castro <rafael@micubiculo.com>
2014-09-18 23:55:09 +0200

    What for?

--- pieces.rb
+++ pieces.rb
@@ -28,23 +28,23 @@ end
 
 class WhitePawn < MoveCoordinates
   def can_move?
-    result =  (@origin_col == @destination_col) && 
-        ((@destination_row == @origin_row+1 && @origin_row > 2) || 
+    (@origin_col == @destination_col) &&
+        ((@destination_row == @origin_row+1 && @origin_row > 2) ||
         (@destination_row <= @origin_row+2 && @origin_row == 2))
   end
 end
 
 class BlackPawn < MoveCoordinates
   def can_move?
-    result =  (@origin_col == @destination_col) && 
-            ((@destination_row == @origin_row-1 && @origin_row < 7) || 
+    (@origin_col == @destination_col) &&
+            ((@destination_row == @origin_row-1 && @origin_row < 7) ||
               (@destination_row >= @origin_row-2 && @origin_row == 7))
   end
 end
 
 class Knight < MoveCoordinates
   def can_move?
-    result = ((@origin_col +1 == @destination_col) && (@origin_row +2 == @destination_row)) ||
+    ((@origin_col +1 == @destination_col) && (@origin_row +2 == @destination_row)) ||
     ((@origin_col +2 == @destination_col) && (@origin_row +1 == @destination_row)) ||
       ((@origin_col +2 == @destination_col) && (@origin_row -1 == @destination_row)) ||
       ((@origin_col +1 == @destination_col) && (@origin_row -2 == @destination_row)) ||
@@ -57,25 +57,25 @@ end
 
 class Rook < MoveCoordinates
   def can_move?
-    result = (@origin_col == @destination_col) || (@origin_row == @destination_row)
+    (@origin_col == @destination_col) || (@origin_row == @destination_row)
   end
 end
 
 class Bishop < MoveCoordinates
   def can_move?
-    result = (@origin_col - @destination_col).abs == (@origin_row - @destination_row).abs
+    (@origin_col - @destination_col).abs == (@origin_row - @destination_row).abs
   end
 end
 
 class Queen < MoveCoordinates
   def can_move?
-    result = Bishop.new(@origin, @destination).can_move? || Rook.new(@origin, @destination).can_move?
+    Bishop.new(@origin, @destination).can_move? || Rook.new(@origin, @destination).can_move?
   end
 end
 
 class King < MoveCoordinates
   def can_move?
-    result = (@origin_col - @destination_col).abs <= 1 && (@origin_row - @destination_row).abs <= 1
+    (@origin_col - @destination_col).abs <= 1 && (@origin_row - @destination_row).abs <= 1
   end
 end
 
